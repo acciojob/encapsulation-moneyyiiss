76c11ec9bfc904476bca0d352ac6adadb1554beb
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-07-13 20:20:15 +0530

    Solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly rwOnly = new RWOnly();
+
+    // error - name has private access in RWOnly
+    // rwOnly.name = "Manish";
+    
+    
+    rwOnly.setName("Manish"); // Set value using the setter function
+    System.out.println(rwOnly.getName()); // Access value using the getter function
+
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+   
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    // setter
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
